366691d02d6e01590c265c0d2972c28d5d9f85eb
hzh <727172424@qq.com>
2022-11-30 17:18:55 +0800

    优化代码

--- es-plus-core/src/main/java/com/es/plus/properties/EsIndexParam.java
+++ es-plus-core/src/main/java/com/es/plus/properties/EsIndexParam.java
@@ -2,55 +2,32 @@ package com.es.plus.properties;
 
 
 import com.es.plus.pojo.EsSettings;
+import lombok.Data;
 
 import java.util.Map;
 
+@Data
 public class EsIndexParam {
-
+    /**
+     * 索引名 创建的索引后缀+S0或者S1 但这里存的默认和别名一样
+     */
     private String index;
+    /**
+     * 索引别名  默认index就是索引别名
+     */
     private String alias;
+    /**
+     * 索引配置
+     */
     private EsSettings esSettings;
+    /**
+     * 索引映射
+     */
     private Map<String, Object> mappings;
+    /**
+     * 子索引的类
+     */
     private Class<?> childClass;
 
 
-    public Class<?> getChildClass() {
-        return childClass;
-    }
-
-    public void setChildClass(Class<?> childClass) {
-        this.childClass = childClass;
-    }
-
-    public Map<String, Object> getMappings() {
-        return mappings;
-    }
-
-    public void setMappings(Map<String, Object> mappings) {
-        this.mappings = mappings;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public String getIndex() {
-        return index;
-    }
-
-    public void setIndex(String index) {
-        this.index = index;
-    }
-
-    public EsSettings getEsSettings() {
-        return esSettings;
-    }
-
-    public void setEsSettings(EsSettings esSettings) {
-        this.esSettings = esSettings;
-    }
 }
