07ae5f5258038511fa7a95d4f6329e6814a70115
hzh <727172424@qq.com>
2022-12-01 10:32:17 +0800

    简化agg语法

--- es-plus-core/src/main/java/com/es/plus/core/wrapper/core/EsExtendsWrapper.java
+++ es-plus-core/src/main/java/com/es/plus/core/wrapper/core/EsExtendsWrapper.java
@@ -6,29 +6,61 @@ import org.elasticsearch.action.search.SearchType;
 
 public interface EsExtendsWrapper<Children, R> {
 
+    /**
+     * 选择字段
+     *
+     */
     EsSelect getSelect();
 
+    /**
+     * 路由
+     *
+     * @param routings 路由
+     * @return {@link Children 本身}
+     */
     Children routings(String... routings);
-
+    /**
+     * 包含字段
+     */
     Children includes(R... func);
-
+    /**
+     * 包含字段
+     */
     Children includes(String... names);
-
+    /**
+     * 忽略字段
+     */
     Children excludes(R... func);
-
+    /**
+     * 忽略字段
+     */
     Children excludes(String... names);
-
+    /**
+     * 排序
+     */
     Children orderBy(String order, R... columns);
-
+    /**
+     * 排序
+     */
     Children orderBy(String order, String... columns);
-
+    /**
+     * 排序
+     */
     Children orderByAsc(String... columns);
-
+    /**
+     * 排序
+     */
     Children orderByDesc(String... columns);
-
+    /**
+     * es查询类型
+     */
     Children searchType(SearchType searchType);
-
+    /**
+     * 高亮字段
+     */
     Children highLight(String field);
-
+    /**
+     *高亮字段
+     */
     Children highLight(String field, String preTag, String postTag);
 }
