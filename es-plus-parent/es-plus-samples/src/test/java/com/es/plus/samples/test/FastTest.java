9c9306a6c4b847637e232cb1c066230c72a7ae12
hzh <727172424@qq.com>
2024-10-16 15:28:55 +0800

    Es聚合查询优化

--- es-plus-parent/es-plus-samples/src/test/java/com/es/plus/samples/test/FastTest.java
+++ es-plus-parent/es-plus-samples/src/test/java/com/es/plus/samples/test/FastTest.java
@@ -74,7 +74,10 @@ public class FastTest {
     public void aaa() {
         EsChainLambdaQueryWrapper<FastTestDTO> fastTestDTOEsChainLambdaQueryWrapper = Es.chainLambdaQuery(FastTestDTO.class);
         EsAggWrapper<FastTestDTO> aggWrapper = fastTestDTOEsChainLambdaQueryWrapper.esAggWrapper();
-        aggWrapper.terms("username",a-> a.size(10000).order(BucketOrder.aggregation("id_max",true)))
+        aggWrapper.terms("username",a-> {
+                  return   a.size(10000).order(BucketOrder.aggregation("id_max",true));
+                        }
+                )
        .subAggregation(es->es.max("id"));
     //例子
 //        aggWrapper.filter("a",()-> this.fastTestService.esQueryWrapper().term("",""));
