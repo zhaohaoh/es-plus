3d205ea49ac927470edc9e1d28fdc3f5684a517b
hzh <727172424@qq.com>
2025-05-15 17:11:25 +0800

    es

--- es-plus-parent/es-plus-common/src/main/java/com/es/plus/adapter/util/LogUtil.java
+++ es-plus-parent/es-plus-common/src/main/java/com/es/plus/adapter/util/LogUtil.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 public class LogUtil {
     
     public static String logSubstring(String jsonStr) {
-        String res = StringUtils.substring(jsonStr, 0, 2000) + ".....省略日志";
+        String res = StringUtils.substring(jsonStr, 0, 10000) + ".....省略日志";
         return res;
     }
     
